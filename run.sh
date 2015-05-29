d492851e1636fd8fab12fcad09d320d6ffefc6d1
Indy Beak <iwb@cognitivescale.com>
2015-05-29 11:14:58 -0500

    overriding cluster.name if ${ELASTICSEARCH_CLUSTER_NAME} is given

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #
+CLUSTER_NAME=${ELASTICSEARCH_CLUSTER_NAME:-elasticsearch}
 
 [[ -d /data/elasticsearch ]] || mkdir -p /data/elasticsearch
 cd /opt/elasticsearch-${ES_VER}
-./bin/elasticsearch --path.data /data/elasticsearch | tee /logs/elasticsearch.log
+./bin/elasticsearch --path.data /data/elasticsearch --cluster.name=$CLUSTER_NAME | tee /logs/elasticsearch.log
